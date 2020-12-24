467921eed8d3baaaceabb6b7d89d85edc79a038e
eric <48635802+ericziethen@users.noreply.github.com>
2020-12-24 16:03:08 +1100

    misc

--- scripts/iptvcat/convert_iptvcat_scraper_files.py
+++ scripts/iptvcat/convert_iptvcat_scraper_files.py
@@ -4,30 +4,23 @@ import argparse
 import os
 import sys
 
-sys.path.append(R'D:\# Eric Projects\ez-m3u8-creator')
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 
-from ez_m3u8_creator import m3u8
+from ez_m3u8_creator.iptvcat import iptvcat_scraper_converter
+from ez_m3u8_creator import project_logger
+
 
 def main():
     """Run the main function."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('m3u8_in_dir', help='The directory to read the m3u8 files.')
+    parser.add_argument('json_dir', help='The directory containing the Json files.')
+    parser.add_argument('m3u8_dir', help='The directory to store the m3u8 files.')
     args = parser.parse_args()
-    parser.add_argument('m3u8_out_dir', help='The directory to store the m3u8 files.')
-    args = parser.parse_args()
 
-    if (not os.path.exists(args.m3u8_in_dir)) or (not os.path.isdir(args.m3u8_in_dir)):
-        raise ValueError(F'"{args.m3u8_in_dir}" is not a vaid directory')
-    if (not os.path.exists(args.m3u8_out_dir)) or (not os.path.isdir(args.m3u8_out_dir)):
-        raise ValueError(F'"{args.m3u8_out_dir}" is not a vaid directory')
-
-    for root, _, files in os.walk(args.m3u8_in_dir):
-        for filename in files:
-            if not filename.lower().endswith('.m3u8') or not filename.lower().endswith('.m3u'):
-                continue
-
-            m3u8_file = m3u8.M3U8File(os.path.join(args.m3u8_in_dir, filename))
-
+    if (not os.path.exists(args.json_dir)) or (not os.path.isdir(args.json_dir)):
+        raise ValueError(F'"{args.json_dir}" is not a vaid directory')
+    if (not os.path.exists(args.m3u8_dir)) or (not os.path.isdir(args.m3u8_dir)):
+        raise ValueError(F'"{args.m3u8_dir}" is not a vaid directory')
 
     iptvcat_scraper_converter.convert_json_dir_to_m3u8(
         in_dir=args.json_dir, out_dir=args.m3u8_dir
