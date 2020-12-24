4b0fcee5c141035ad1d82ea4b0ab8cd77f982632
eric <48635802+ericziethen@users.noreply.github.com>
2020-12-24 13:00:59 +1100

    load file

--- ez_m3u8_creator/m3u8.py
+++ ez_m3u8_creator/m3u8.py
@@ -1,15 +1,18 @@
 """Code to handle m3u8 file type."""
 
 M3U8_OPENING_TAG = '#EXTM3U'
-M3U8_CHANNEL_INFO_PREFIX = '#EXTINF:0'
+M3U8_CHANNEL_INFO_PREFIX = '#EXTINF:'
 
 
 class M3U8File():
     """An m3u8 file representation."""
 
-    def __init__(self):
+    def __init__(self, file_path=None):
         """Initialize the M3U8 file."""
         self.channel_list = []
+
+        if file_path is not None:
+            self._load_file(file_path)
 
     def add_channel(self, *, name, url):
         """Add a channel to the file."""
@@ -18,10 +21,25 @@ class M3U8File():
             'url': url
         })
 
+    def _load_file(self, file_path):
+        with open(file_path, 'r', encoding='utf-8') as file_ptr:
+            name = None
+            for line in file_ptr:
+                line = line.rstrip()
+                if line.startswith(M3U8_OPENING_TAG):
+                    pass
+                elif line.startswith(M3U8_CHANNEL_INFO_PREFIX):
+                    idx = line.rfind(',')
+                    if idx >= 0:
+                        name = line[idx + 1:]  # New str from after the last comma
+                else:  # Assume it's the url
+                    self.add_channel(name=name, url=line)
+                    name = None
+
     def write_file(self, file_path):
         """Write the m3u8 file."""
         with open(file_path, 'w', encoding='utf-8') as file_ptr:
             file_ptr.write(F'{M3U8_OPENING_TAG}\n')
             for channel in self.channel_list:
-                file_ptr.write(F'{M3U8_CHANNEL_INFO_PREFIX},{channel["name"]}\n')
+                file_ptr.write(F'{M3U8_CHANNEL_INFO_PREFIX}0,{channel["name"]}\n')
                 file_ptr.write(F'{channel["url"]}\n')
--- tests/ez_m3u8_creator/test_m3u8.py
+++ tests/ez_m3u8_creator/test_m3u8.py
@@ -1,6 +1,8 @@
 
 from ez_m3u8_creator import m3u8
 
+TEST_FILE_INTEGRATION_CONVERTED_PATH = R'tests/ez_m3u8_creator/TestFiles/test_file_integration_converted.m3u8'
+
 
 def test_write_m3u8_file(tmpdir):
     m3u8_file = m3u8.M3U8File()
@@ -18,3 +20,30 @@ def test_write_m3u8_file(tmpdir):
         assert line_list[2].rstrip() == 'channel_url'
         assert line_list[3].rstrip() == '#EXTINF:0,Channel 2'
         assert line_list[4].rstrip() == 'channel_url2'
+
+
+def test_load_m3u8_file(tmpdir):
+    m3u8_file = m3u8.M3U8File(TEST_FILE_INTEGRATION_CONVERTED_PATH)
+    out_file = tmpdir.join('output.m3u8')
+    m3u8_file.write_file(out_file)
+
+    with open(TEST_FILE_INTEGRATION_CONVERTED_PATH, 'r', encoding='utf-8') as file_ptr:
+        in_file_line_list = list(file_ptr)
+        out_file_line_list = out_file.read_text(encoding='utf-8').split('\n')
+
+        print(out_file_line_list)
+
+        assert len(in_file_line_list) == 5
+
+        # Rough test that all the lines are matching
+        for idx, line in enumerate(in_file_line_list):
+            assert line.rstrip() == out_file_line_list[idx]
+
+
+def test_get_categories_from_json():
+    assert False
+
+
+def test_add_categories_from_json_to_m3u():
+    assert False
+
